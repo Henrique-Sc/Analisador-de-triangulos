cc5a80429ff08a66c9b105a1fc820d12d80cd4a0
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-31 20:52:00 -0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,8 +1,19 @@
 # Imports
 import functions
+from functions import cores
+
+# Cores / formatação de texto
 
 # Introdução do programa - o que serve
 functions.title('Analisador de triângulos')
+
+print(f'''
+{cores.italico()}Com base no comprimento dos triângulos informados, retorna dados, como: 
+    > tipo de triângulo
+    > área
+    > perímetro
+    > ângulos
+    > Equivalência entre os triângulos informados.{cores.reset()}''')
 
 # Quantidades de triângulos que vai analisar
 
