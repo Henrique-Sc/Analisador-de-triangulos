f0e6bd8a54c5928ef74c7c4b327d480885cb8b30
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-31 20:40:18 -0300

    Update main.py

---
+++ program/main.py
@@ -0,0 +1,15 @@
+# Imports
+import functions
+
+# Introdução do programa - o que serve
+functions.title('Analisador de triângulos')
+
+# Quantidades de triângulos que vai analisar
+
+# Fazer uma opção para ver uma imagem de exemplo para visualizar as informações
+
+# Inserir dados do triângulo
+
+# Análise do triângulo
+
+# Mostrar na tela o resultado
