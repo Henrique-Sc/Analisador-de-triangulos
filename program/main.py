6e3985d034b38e0abfa8f6d4e552a1fde3e786f0
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-31 21:19:01 -0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,21 +1,29 @@
-# Imports
-import functions
-from functions import cores
+# Imports e cores
+from functions import dados, format
+from time import sleep
 
-# Cores / formatação de texto
+reset = '\033[m'
+italico = '\033[3m'
 
 # Introdução do programa - o que serve
-functions.title('Analisador de triângulos')
+format.title('Analisador de triângulos', cor=3)
 
-print(f'''
-{cores.italico()}Com base no comprimento dos triângulos informados, retorna dados, como: 
+sleep(1)
+
+print(f'''{italico}
+Com base no comprimento dos triângulos informados, retorna dados, como: 
     > tipo de triângulo
     > área
     > perímetro
     > ângulos
-    > Equivalência entre os triângulos informados.{cores.reset()}''')
+    > Equivalência entre os triângulos informados {reset}\n''')
+
+sleep(1)
 
 # Quantidades de triângulos que vai analisar
+quant_triang = dados.leiaInt('-> Quantos triângulos deseja analisar? ')
+
+print(quant_triang)
 
 # Fazer uma opção para ver uma imagem de exemplo para visualizar as informações
 
