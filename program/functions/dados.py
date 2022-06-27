252b05ef24d5fd1c9dfd76dfdb5e31487fe3f7c1
Etec <etec@etec.com>
2022-06-27 16:29:38 -0300

    Update dados.py

--- program/functions/dados.py
+++ program/functions/dados.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def leiaInt(t=''):
     while True:
         txt = input(t)
@@ -9,3 +11,24 @@ def leiaInt(t=''):
             print('\033[31mValor inválido! Digite um número inteiro.\033[m')
 
     return num
+
+
+def receberTriang(txt):
+    quantTriang = leiaInt(txt)
+
+    if quantTriang <= 0:
+        print(f'\nDeseja realmente analisar \"{quantTriang}\" triângulos? isso fará com que o programa não execute a '
+              f'análise.')
+        quantTriang = int(input('Quantos triângulos deseja analizar? ').strip())
+
+
+def escolha(txt):
+    esc = input(txt).strip().upper()[0]
+
+    if esc not in 'SN':
+        print()
+    while esc not in 'SN':
+        esc = input('Valor inválido! Digite S [Sim] ou N [Não]: ').strip().upper()[0]
+
+    if esc == 'S':
+        Image.open('../imagens/triangulo.png').show()
