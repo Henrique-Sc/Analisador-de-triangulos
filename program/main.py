ac33fa9f9df47d427bcd80efd039ddd7f7e4e5cc
Etec <etec@etec.com>
2022-06-27 16:38:57 -0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
 # Imports e cores
 from functions import dados, format
 from time import sleep
+from PIL import Image
 
 reset = '\033[m'
 italico = '\033[3m'
@@ -22,8 +23,14 @@ sleep(1)
 
 # Quantidades de triângulos que vai analisar
 quant_triang = dados.receberTriang('-> Quantos triângulos deseja analisar? ')
+# Fazer se o usuário digitar um número menor ou igual 0
 
 # Fazer uma opção para ver uma imagem de exemplo para visualizar as informações
+esc = dados.escolha('\nDeseja visualizar uma imagem de exemplo para facilitar a inserção e leitura dos dados? S [Sim] '
+                    'ou N [Não]: ')
+
+if esc:
+    Image.open("../imagens/triangulo.png")
 
 
 # Inserir dados do triângulo
