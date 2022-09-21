e463713f14aeb0a06afe0f9c894298163a3abbb8
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-09-21 13:28:53 -0300

    Update format.py

--- program/functions/format.py
+++ program/functions/format.py
@@ -1,8 +1,14 @@
 formatacao = [
-    '\033[m',    # 0 - Reset
-    '\033[3m',   # 1 - Itálico
-    '\033[31m',  # 2 - Vermelho
-    '\033[34m'   # 3 - Azul
+    '\033[m',         # 0 - Reset
+    '\033[1m',        # 1 - Negrito
+    '\033[3m',        # 2 - Itálico
+    '\033[38;5;9m',   # 3 - Vermelho
+    '\033[38;5;10m',  # 4 - Verde
+    '\033[38;5;11m',  # 5 - Amarelo
+    '\033[38;5;12m',  # 6 - Azul
+    '\033[38;5;13m',  # 7 - Magenta
+    '\033[38;5;14m',  # 8 - Ciano
+    '\033[38;5;15m'   # 9 - Branco
 ]
 
 
