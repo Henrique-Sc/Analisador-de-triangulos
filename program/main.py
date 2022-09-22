608690bc6f3347fcb5a90ac168f28ec61311fa8e
Etec <etec@etec.com>
2022-09-22 17:22:15 -0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -21,101 +21,93 @@ while True:
 
         # Perguntar ao usuário quantos triângulos ele quer analizar
         quant_triang = dados.receberTriang(msg='\nQuantos triângulos deseja analizar? ')
+        sleep(1)
 
-        if quant_triang == 0:
-            print('BREAK')
+        print()
+        format.linha()
+
+        # Declaração das variáveis
+        triangs = []
+        temp = []
+
+        # Fazer uma opção para ver uma imagem de exemplo para visualizar as informações
+        print(f'\nDeseja visualizar uma {format.formatacao[6]}imagem de exemplo{format.formatacao[0]}, \n'
+              f'para facilitar a inserção e leitura dos dados?\n')
+        sleep(3)
+        if dados.escolha():
+            Image.open('images/triangle.png').show()
+
+        # Laço para inserir os dados sobre os triângulos
+        for c in range(0, quant_triang):
+            sleep(0.5)
+
+            # Título
+            print()
+            print('=' * 37)
+            print(f'\n{f" {c + 1}º Triângulo ":-^37}')
+
+            # Entrada dos dados
+            print('\nDigite as informações: ')
+            print()
+            temp.append(float(input('Lado A: ').strip()))
+            temp.append(float(input('Lado B: ').strip()))
+            temp.append(float(input('Lado C: ').strip()))
+
+            # Inserindo os dados e limpando a lista temp
+            triangs.append(temp[:])
+            temp.clear()
+
+            print()
+            print('=' * 37)
 
         sleep(0.5)
+        print('\n\n=-=-=-=-= Análise dos dados =-=-=-=-=\n')
 
-        if quant_triang >= 1:
-            # Declaração das variáveis
-            triangs = []
-            temp = []
+        # Resultado
+        for i, triang in enumerate(triangs):
+            sleep(1.5)
+            print()
+            print('=' * 37)
+            print(f'\n{f" {i + 1}º Triângulo ":-^37}')
+            print()
 
-            # Fazer uma opção para ver uma imagem de exemplo para visualizar as informações
-            esc = input(
-                '\nDeseja visualizar uma imagem de exemplo para facilitar a inserção e leitura dos dados? S [Sim] ou N [Não]: ').strip().upper()[
-                0]
+            # Criando as variáveis a, b e c para ficar de fácil acesso
+            a = triang[0]
+            b = triang[1]
+            c = triang[2]
 
-            if esc not in 'SN':
-                print()
-            while esc not in 'SN':
-                esc = input('Valor inválido! Digite S [Sim] ou N [Não]: ').strip().upper()[0]
+            # Se com os dados não for possível fazer um triângulo, não realizará os cálculos
+            if a + b > c and a + c > b and b + c > a:
+                # Cálculos
+                s = (a + b + c) / 2  # S - Para realizar a equação de Heron
+                area = sqrt(s * (s - a) * (s - b) * (s - c))  # Área
+                peri = a + b + c  # Perímetro
 
-            if esc == 'S':
-                Image.open('../imagens/triangulo.png').show()
+                angAB = degrees(arccos((a ** 2 + b ** 2 - c ** 2) / (2 * b * a)))  # Ângulo AB
+                angBC = degrees(arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))  # Ângulo BC
+                angAC = degrees(arccos((a ** 2 + c ** 2 - b ** 2) / (2 * a * c)))  # Ângulo AC
 
-            # Laço para inserir os dados sobre os triângulos
-            for c in range(0, quant_triang):
-                sleep(0.5)
+                altA = (2 * area) / a  # Altura A
+                altB = (2 * area) / b  # Altura B
+                altC = (2 * area) / c  # Altura C
 
-                # Título
-                print()
-                print('=' * 37)
-                print(f'\n{f" {c + 1}º Triângulo ":-^37}')
-
-                # Entrada dos dados
-                print('\nDigite as informações: ')
-                print()
-                temp.append(float(input('Lado A: ').strip()))
-                temp.append(float(input('Lado B: ').strip()))
-                temp.append(float(input('Lado C: ').strip()))
-
-                # Inserindo os dados e limpando a lista temp
-                triangs.append(temp[:])
-                temp.clear()
-
-                print()
-                print('=' * 37)
-
-            sleep(0.5)
-            print('\n\n=-=-=-=-= Análise dos dados =-=-=-=-=\n')
-
-            # Resultado
-            for i, triang in enumerate(triangs):
-                sleep(1.5)
-                print()
-                print('=' * 37)
-                print(f'\n{f" {i + 1}º Triângulo ":-^37}')
-                print()
-
-                # Criando as variáveis a, b e c para ficar de fácil acesso
-                a = triang[0]
-                b = triang[1]
-                c = triang[2]
-
-                # Se com os dados não for possível fazer um triângulo, não realizará os cálculos
-                if a + b > c and a + c > b and b + c > a:
-                    # Cálculos
-                    s = (a + b + c) / 2  # S - Para realizar a equação de Heron
-                    area = sqrt(s * (s - a) * (s - b) * (s - c))  # Área
-                    peri = a + b + c  # Perímetro
-
-                    angAB = degrees(arccos((a ** 2 + b ** 2 - c ** 2) / (2 * b * a)))  # Ângulo AB
-                    angBC = degrees(arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))  # Ângulo BC
-                    angAC = degrees(arccos((a ** 2 + c ** 2 - b ** 2) / (2 * a * c)))  # Ângulo AC
-
-                    altA = (2 * area) / a  # Altura A
-                    altB = (2 * area) / b  # Altura B
-                    altC = (2 * area) / c  # Altura C
-
-                    # Saída
-                    if a == b == c:
-                        print('Tipo: Equilátero')
-                    elif a == b or b == c or a == c:
-                        print('Tipo: Isósceles')
-                    else:
-                        print('Tipo: Escaleno')
-
-                    print(f'\nÁrea: {area:.2f} | Perímetro: {peri:.2f}')
-                    print(f'ângulo AB: {angAB:.2f}° | ângulo BC: {angBC:.2f}° | ângulo AC: {angAC:.2f}°')
-                    print(f'Altura A: {altA:.2f} | Altura B: {altB:.2f} | Altura C: {altC:.2f}')
-
+                # Saída
+                if a == b == c:
+                    print('Tipo: Equilátero')
+                elif a == b or b == c or a == c:
+                    print('Tipo: Isósceles')
                 else:
-                    print('Com esses dados, não é possível fazer um triângulo!')
-                print()
-                print('=' * 37)
-                print()
+                    print('Tipo: Escaleno')
+
+                print(f'\nÁrea: {area:.2f} | Perímetro: {peri:.2f}')
+                print(f'ângulo AB: {angAB:.2f}° | ângulo BC: {angBC:.2f}° | ângulo AC: {angAC:.2f}°')
+                print(f'Altura A: {altA:.2f} | Altura B: {altB:.2f} | Altura C: {altC:.2f}')
+
+            else:
+                print('Com esses dados, não é possível fazer um triângulo!')
+            print()
+            print('=' * 37)
+            print()
         sleep(1.5)
 
         print('\nFim da execução. Volte sempre (•◡•)/')
