f0387bfd172b36d341afd386778f291018dedb5c
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-10-16 16:15:29 -0300

    Update __init__.py

--- program/functions/analisarTriang/__init__.py
+++ program/functions/analisarTriang/__init__.py
@@ -1,5 +1,12 @@
 from ..format import *
-
+from math import sqrt, degrees
+from numpy import arccos
 
 def analise(dados):
-    
+    for i, t in enumerate(dados):
+        title(f'{i + 1}º triângulo', cor=3)
+        sleep(1)
+
+
+        # de um triangulo pro outro
+        sleep(1.5)
