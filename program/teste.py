dcd4d6b5d7d23bdfaa6d452a5f2c964887536aed
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-11-18 14:29:29 -0300

    Update teste.py

--- program/teste.py
+++ program/teste.py
@@ -1,7 +1,7 @@
 # from functions import format, dados
 from functions.analisarTriang import *
 
-analise([(10, 10, 10), (10, 12, 50)])
+analise([(10, 10, 10)])
 
 # format.subtile('Análise dos dados', cor=3)
 
