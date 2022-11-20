c68be435acc7ccb7dede2904f59cb07fabf28caa
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-11-20 00:02:49 -0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -50,51 +50,6 @@ while True:
         print()
         analisarTriang.analise(dados_Triangs=dados_Triangs)
 
-        # # Resultado
-        # for i, triang in enumerate(triangs):
-        #     sleep(1.5)
-        #     print()
-        #     print('=' * 37)
-        #     print(f'\n{f" {i + 1}º Triângulo ":-^37}')
-        #     print()
-        #
-        #     # Criando as variáveis a, b e c para ficar de fácil acesso
-        #     a = triang[0]
-        #     b = triang[1]
-        #     c = triang[2]
-        #
-        #     # Se com os dados_Triangs não for possível fazer um triângulo, não realizará os cálculos
-        #     if a + b > c and a + c > b and b + c > a:
-        #         # Cálculos
-        #         s = (a + b + c) / 2  # S - Para realizar a equação de Heron
-        #         area = sqrt(s * (s - a) * (s - b) * (s - c))  # Área
-        #         peri = a + b + c  # Perímetro
-        #
-        #         angAB = degrees(arccos((a ** 2 + b ** 2 - c ** 2) / (2 * b * a)))  # Ângulo AB
-        #         angBC = degrees(arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))  # Ângulo BC
-        #         angAC = degrees(arccos((a ** 2 + c ** 2 - b ** 2) / (2 * a * c)))  # Ângulo AC
-        #
-        #         altA = (2 * area) / a  # Altura A
-        #         altB = (2 * area) / b  # Altura B
-        #         altC = (2 * area) / c  # Altura C
-        #
-        #         # Saída
-        #         if a == b == c:
-        #             print('Tipo: Equilátero')
-        #         elif a == b or b == c or a == c:
-        #             print('Tipo: Isósceles')
-        #         else:
-        #             print('Tipo: Escaleno')
-        #
-        #         print(f'\nÁrea: {area:.2f} | Perímetro: {peri:.2f}')
-        #         print(f'ângulo AB: {angAB:.2f}° | ângulo BC: {angBC:.2f}° | ângulo AC: {angAC:.2f}°')
-        #         print(f'Altura A: {altA:.2f} | Altura B: {altB:.2f} | Altura C: {altC:.2f}')
-        #
-        #     else:
-        #         print('Com esses dados, não é possível fazer um triângulo!')
-        #     print()
-        #     print('=' * 37)
-        #     print()
         sleep(1.5)
 
         print('\nFim da execução. Volte sempre (•◡•)/')
